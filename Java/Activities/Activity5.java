1667531e9e985821daf23190cf3e517952f13b5f
Komal3315 <42232751+Komal3315@users.noreply.github.com>
2025-07-27 12:41:36 +0530

    Add files via upload

---
+++ Java/Activities/Activity5.java
@@ -0,0 +1,13 @@
+package activities;
+
+public class Activity5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyBook b = new MyBook();
+		b.setTitle("My New Novel");
+		System.out.println(b.getTitle());
+
+	}
+
+}
