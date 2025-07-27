3c984ea5c904832ec627ddf219fcccc0c8c7a4ec
Komal3315 <42232751+Komal3315@users.noreply.github.com>
2025-07-27 19:32:17 +0530

    Add files via upload

---
+++ Java/Activities/MountainBike.java
@@ -0,0 +1,19 @@
+package activities;
+
+public class MountainBike extends Bicyle {
+
+	MountainBike(int gears, int currentSpeed) {
+		super(gears, currentSpeed);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void setHeight(int newValue) {
+	    int seatHeight = newValue;
+	}
+	
+	public String bicycleDesc() {
+		return null;
+        
+    }
+
+}
